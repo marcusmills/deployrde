c7e4453ef80958f30caeaa0ac645c50803fc0d15
Marcus Mills <marcus.mills@avadolearning.com>
2021-12-22 19:31:27 +0000

    Update choco install scripts

--- shdt-deploy.ps1
+++ shdt-deploy.ps1
@@ -4,5 +4,22 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install chocolatey packages
-choco upgrade powerbi adobereader AdoptOpenJDK GoogleChrome nessus-agent notepadplusplus -y
 
+## Install PowerBI
+choco install powerbi -y --ignore-checksums
+
+## Install Adobe Reader
+choco install adobereader -y
+
+## Install Java
+choco install AdoptOpenJDK -y
+
+## Install Google Chrome
+choco install GoogleChrome -y
+
+## Install Notepad++
+choco install notepadplusplus -y
+choco install notepadplusplus-npppluginmanager -y
+
+# Install 7zip
+choco install 7zip  -y
