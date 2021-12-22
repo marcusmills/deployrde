2b3fa624ff11612c1eeedf92a949896da2860df2
Marcus Mills <marcus.mills@avadolearning.com>
2021-12-22 17:52:48 +0000

    Add -y to choco install

--- shdt-deploy.ps1
+++ shdt-deploy.ps1
@@ -4,5 +4,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install chocolatey packages
-choco upgrade powerbi adobereader AdoptOpenJDK GoogleChrome nessus-agent notepadplusplus
+choco upgrade powerbi adobereader AdoptOpenJDK GoogleChrome nessus-agent notepadplusplus -y
 
